1945c76fa99debc8d5edef196130b9ec6db4a062
VedranKat <vedstandard@runbox.com>
2020-04-28 00:01:06 +0200

    update 6, add 7

---
+++ LabVjezba_7/ZadatakVjezba7.py
@@ -0,0 +1,45 @@
+from errno import ECONNREFUSED
+from functools import partial
+from multiprocessing import Pool
+import multiprocessing as mp
+import socket
+import time
+import datetime
+
+def ping(host, port):
+    try:
+        socket.socket().connect((host, port))
+        print(str(port) + " - Otvoren") 
+        return       
+    except Exception:
+        return     
+
+
+def scan_ports(host):
+    od = input('Pocetni port: ')
+    do = input('Zadnji port: ')
+    p = Pool(mp.cpu_count()*2)
+    ping_host = partial(ping, host)
+    p.map(ping_host, range(int(od), int(do)))
+
+
+def main():      
+    print("Program je pokrenut na računalu:")
+    print (socket.gethostname())
+    print ("Program pokrenut u:")
+    print (datetime.datetime.now())
+    
+    PocetnoVrime = time.time()
+    target = input('Unesite adresu: ')
+    
+    host = socket.gethostbyname(target)
+    socket.setdefaulttimeout(0.55)
+    
+        
+    scan_ports(host)   
+    trajanje = float("%0.3f" % (time.time() - PocetnoVrime))
+    print("Skeniranje je trajalo: ", trajanje, "sekunde")
+
+
+if __name__ == "__main__":
+    main()
