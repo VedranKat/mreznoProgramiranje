b43e9379e86fb9726ee70f061b9aa65de0d64509
VedranKat <vedstandard@runbox.com>
2020-04-27 18:00:14 +0200

    Vjezba6

---
+++ LabVjezba_6/Primjer3.py
@@ -0,0 +1,57 @@
+#threading_primjer_red_cekanja.py
+# #Mrezno programiranje LABno6 2019
+
+import queue
+import threading
+import time
+
+exitFlag= 0
+
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, q):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.q = q
+    def run(self):
+        print ("Pokrecem nit " + self.name)
+        process_data(self.name, self.q)
+        print("Izlazim iz niti " + self.name)
+
+def process_data(threadName, q):
+    while not exitFlag:
+        queueLock.acquire()
+        if not workQueue.empty():
+            data = q.get()
+            queueLock.release()
+            print ("%s procesuira %s" % (threadName, data))
+        else:
+            queueLock.release()
+            time.sleep(1)
+
+threadList = ["Thread-1", "Thread-2", "Thread-3"]
+nameList = ["Jedan", "Dva", "Tri", "Cetiri", "Pet"]
+queueLock = threading.Lock()
+workQueue = queue.Queue(10)
+threads = []
+threadID = 1
+
+for tName in threadList:
+    thread = myThread(threadID, tName, workQueue)
+    thread.start()
+    threads.append(thread)
+    threadID += 1
+
+queueLock.acquire()
+for word in nameList:
+    workQueue.put(word)
+queueLock.release()
+
+while not workQueue.empty():
+    pass
+
+exitFlag = 1
+
+for t in threads:
+    t.join()
+print ("\nIzlazim iz glavne niti\n")
---
+++ LabVjezba_6/ZadatakVjezbe6.py
@@ -0,0 +1,50 @@
+import socket
+import time 
+import threading  
+import datetime
+from queue import Queue 
+
+socket.setdefaulttimeout(0.55)
+p_lock = threading.Lock()
+
+print("Program je pokrenut na računalu:")
+print (socket.gethostname())
+print ("Program pokrenut u:")
+print (datetime.datetime.now())
+
+target = input('Unesite adresu: ')
+
+t_IP = socket.gethostbyname(target)
+od = input('Pocetni port: ')
+do = input('Zadnji port: ')
+
+def ports(port):   
+   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+   try:      
+      con = sock.connect((t_IP, port)) 
+      with p_lock:
+         print(port, 'je otvoren')  
+      con.close()
+   except:
+      pass
+
+def threads():
+   while True:    
+      worker = q.get()      
+      ports(worker)      
+      q.task_done()
+     
+q = Queue()
+PocetnoVrime = time.time()
+ 
+for x in range(200):   
+   t = threading.Thread(target = threads)
+   t.daemon = True
+   t.start()
+
+for worker in range(int(od), int(do)):
+   q.put(worker)
+  
+q.join()
+trajanje = float("%0.3f" % (time.time() - PocetnoVrime))
+print("Skeniranje je trajalo: ", trajanje, "sekunde")
